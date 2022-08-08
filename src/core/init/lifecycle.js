1b0dd1aa463996b9f8c60f3ad07297b8a4d34286
Xunwtlt <75558996+xunwtlt@users.noreply.github.com>
2022-08-08 14:56:16 +0800

    Add files via upload

---
+++ src/core/init/lifecycle.js
@@ -0,0 +1,57 @@
+import { noop } from '../util/core';
+
+/** @typedef {import('../Docsify').Constructor} Constructor */
+
+/**
+ * @template {!Constructor} T
+ * @param {T} Base - The class to extend
+ */
+export function Lifecycle(Base) {
+  return class Lifecycle extends Base {
+    initLifecycle() {
+      const hooks = [
+        'init',
+        'mounted',
+        'beforeEach',
+        'afterEach',
+        'doneEach',
+        'ready',
+      ];
+
+      this._hooks = {};
+      this._lifecycle = {};
+
+      hooks.forEach(hook => {
+        const arr = (this._hooks[hook] = []);
+        this._lifecycle[hook] = fn => arr.push(fn);
+      });
+    }
+
+    callHook(hookName, data, next = noop) {
+      const queue = this._hooks[hookName];
+
+      const step = function(index) {
+        const hookFn = queue[index];
+
+        if (index >= queue.length) {
+          next(data);
+        } else if (typeof hookFn === 'function') {
+          if (hookFn.length === 2) {
+            hookFn(data, result => {
+              data = result;
+              step(index + 1);
+            });
+          } else {
+            const result = hookFn(data);
+            data = result === undefined ? data : result;
+            step(index + 1);
+          }
+        } else {
+          step(index + 1);
+        }
+      };
+
+      step(0);
+    }
+  };
+}
